5c63157d43a4c867ef0aa22342960b0afa175cfb
Thiago Farina <tfransosi@gmail.com>
2026-03-18 10:01:00 +0000

    refactor: localize and structure GPL license text in about dialog
    Move the GPL license text from file scope into show_about_dialog()
    to reduce its visibility and better encapsulate dialog-related data.
    
    Split the license into paragraph-sized strings and build the final
    text at runtime using g_strconcat(). This improves readability and
    prepares the text for translation by keeping logical sections
    separate.
    
    Free the constructed string after use to avoid leaks.
    
    Signed-off-by: Thiago Farina <tfransosi@gmail.com>

--- src/about_dialog.c
+++ src/about_dialog.c
@@ -4,21 +4,9 @@
 
 #include "config.h"
 
-static char const license_gpl2[] =
-    "This program is free software: you can redistribute it and/or modify\n"
-    "it under the terms of the GNU General Public License as published by\n"
-    "the Free Software Foundation; either version 2 of the License, or\n"
-    "(at your option) any later version.\n"
-    "\n"
-    "This program is distributed in the hope that it will be useful,\n"
-    "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
-    "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
-    "GNU General Public License for more details.\n"
-    "\n"
-    "You should have received a copy of the GNU General Public License\n"
-    "along with this program. If not, see <https://www.gnu.org/licenses/>.";
-
 void show_about_dialog(GtkWindow *parent) {
+  char *license;
+
   /* List of authors */
   static char const *authors[] = {
     NULL
@@ -32,6 +20,24 @@ void show_about_dialog(GtkWindow *parent) {
   static char const comments[] = "A simple and easy to use address book manager";
   static char const copyright[] = "Copyright © 2019";
 
+  static char const *license_part[] = {
+      "This is free software; you can redistribute it and/or modify\n"
+      "it under the terms of the GNU General Public License as published by\n"
+      "the Free Software Foundation; either version 2 of the License, or\n"
+      "(at your option) any later version.\n",
+      "This is distributed in the hope that it will be useful,\n"
+      "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+      "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+      "GNU General Public License for more details.\n",
+      "You should have received a copy of the GNU General Public License\n"
+      "along with this program; if not, see <https://www.gnu.org/licenses/>.\n"
+  };
+
+  license = g_strconcat(license_part[0], "\n",
+			license_part[1], "\n",
+			license_part[2], "\n",
+			NULL);
+
   gtk_show_about_dialog(parent,
                         "program-name",       "Tribe",
 			"version",            VERSION,
@@ -40,7 +46,9 @@ void show_about_dialog(GtkWindow *parent) {
 			"authors",            authors,
 			"documenters",        documenters,
 			"translator-credits", "",
-			"license",            license_gpl2,
+			"license",            license,
 			"website",            "https://tribe.infinityfreeapp.com",
 			NULL);
+
+  g_free(license);
 }
